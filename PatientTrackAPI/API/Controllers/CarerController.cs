c2e6f6d16d2d65c6fcd1bd0e536982d79519819e
JoeScho <joeschofield@live.co.uk>
2017-03-01 18:53:11 +0000

    Working API!

---
+++ PatientTrackAPI/API/Controllers/CarerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using API.Models;
+
+namespace API.Controllers
+{
+    [EnableCors(origins: "http://localhost:55058", headers: "*", methods: "*")]
+    public class CarerController : ApiController
+    {
+        // GET api/carers
+        [Route("api/carers")]
+        public HttpResponseMessage Get()
+        {
+            var carers = CarerModel.GetAllCarers();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, carers);
+            return response;
+        }
+
+        // GET api/carer/5
+        [Route("api/carer/{id?}")]
+        public HttpResponseMessage Get(int id)
+        {
+            var carers = CarerModel.GetCarer(id);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, carers);
+            return response;
+        }
+
+        // GET api/carer/John
+        [Route("api/carer/{name:alpha}")]
+        public HttpResponseMessage Get(string name)
+        {
+            var carers = CarerModel.SearchCarerByName(name);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, carers);
+            return response;
+        }
+
+        // POST api/carers
+        [Route("api/carers")]
+        public HttpResponseMessage Post(Carer c)
+        {
+            var carers = CarerModel.InsertCarer(c);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, carers);
+            return response;
+        }
+
+        // PUT api/carers
+        [Route("api/carers")]
+        public HttpResponseMessage Put(Carer c)
+        {
+            var carers = CarerModel.UpdateCarer(c);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, carers);
+            return response;
+        }
+
+        // DELETE api/carers
+        [Route("api/carers")]
+        public HttpResponseMessage Delete(Carer c)
+        {
+            var carers = CarerModel.DeleteCarer(c);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, carers);
+            return response;
+        }
+    }
+}
---
+++ PatientTrackAPI/API/Models/CarerModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class CarerModel
+    {
+        private static PatientTrackDBEntities dataContext = new PatientTrackDBEntities();
+
+        public static List<Carer> GetAllCarers()
+        {
+            try
+            {
+                var query = from carer in dataContext.Carers
+                            select carer;
+                return query.ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return null;
+            }
+        }
+
+        public static Carer SearchCarerByName(string carerName)
+        {
+            try
+            {
+                var query = from carer in dataContext.Carers
+                            where carer.CarerName.Contains(carerName)
+                            select carer;
+                return query.First();
+            }            
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return null;
+            }
+        }
+
+        public static Carer GetCarer(int CarerID)
+        {            
+            try
+            {
+                var query = from carer in dataContext.Carers
+                            where carer.CarerID == CarerID
+                            select carer;
+                return query.First();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return null;
+            }
+        }
+
+        public static List<Carer> InsertCarer(Carer c)
+        {            
+            try
+            {
+                dataContext.Carers.Add(c);
+                dataContext.SaveChanges();
+                return GetAllCarers();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return null;
+            }
+        }
+
+        public static List<Carer> UpdateCarer(Carer c)
+        {
+            try
+            {
+                var car = (from carer in dataContext.Carers
+                           where carer.CarerID == c.CarerID
+                           select carer).SingleOrDefault();
+                car.CarerName = c.CarerName;
+                car.CarerEmail = c.CarerEmail;
+                car.CarerPwd = c.CarerPwd;
+                dataContext.SaveChanges();
+                return GetAllCarers();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return null;
+            }
+        }
+
+        public static List<Carer> DeleteCarer(Carer c)
+        {
+            try
+            {
+                var car = (from carer in dataContext.Carers
+                           where carer.CarerID == c.CarerID
+                           select carer).SingleOrDefault();
+                dataContext.Carers.Remove(car);
+                dataContext.SaveChanges();
+                return GetAllCarers();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return null;
+            }
+        }
+    }
+}
